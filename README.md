f1a252085a62558090fa41655745e8296e191f68
pgcr71 <pgcr71@gmail.com>
2024-08-28 19:53:48 +0530

    Feat: info to database

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # coasta-unility-management-IOT-mqtt
+
+1) install nodejs from https://nodejs.org/en
+2) change the required options in index.js
+2) npm install
+3) npm run start
+4) server should be listening for all the messages published and should store info in coasta.db sqlite database file
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 
 var mqtt = require('mqtt'); //https://www.npmjs.com/package/mqtt
 var Topic = '#'; //subscribe to all topics
-var Broker_URL = 'mqtt://localhost';
-var Database_URL = 'localhost';
+var Broker_URL = 'localhost';
 
 var options = {
 	clientId: 'MyMQTT',
 	port: 1883,
 	//username: 'mqtt_user',
 	//password: 'mqtt_password',	
-	keepalive : 60
+	keepalive: 60
 };
 
-var client  = mqtt.connect(Broker_URL, options);
+var client = mqtt.connect(Broker_URL, options);
 client.on('connect', mqtt_connect);
 client.on('reconnect', mqtt_reconnect);
 client.on('error', mqtt_error);
@@ -20,23 +19,28 @@ client.on('message', mqtt_messsageReceived);
 client.on('close', mqtt_close);
 
 function mqtt_connect() {
-    //console.log("Connecting MQTT");
-    client.subscribe(Topic, mqtt_subscribe);
+	//console.log("Connecting MQTT");
+	client.subscribe(Topic, mqtt_subscribe);
 };
 
 function mqtt_subscribe(err, granted) {
-    console.log("Subscribed to " + Topic);
-    if (err) {console.log(err);}
+	if (!err) {
+		setTimeout(() => {
+			client.publish("presence", "Hello mqtt");
+		}, 30)
+	}
+	console.log("Subscribed to " + Topic);
+	if (err) { console.log(err); }
 };
 
 function mqtt_reconnect(err) {
-    //console.log("Reconnect MQTT");
-    //if (err) {console.log(err);}
-	client  = mqtt.connect(Broker_URL, options);
+	//console.log("Reconnect MQTT");
+	//if (err) {console.log(err);}
+	client = mqtt.connect(Broker_URL, options);
 };
 
 function mqtt_error(err) {
-    //console.log("Error!");
+	//console.log("Error!");
 	//if (err) {console.log(err);}
 };
 
@@ -46,15 +50,15 @@ function after_publish() {
 
 //receive a message from MQTT broker
 function mqtt_messsageReceived(topic, message, packet) {
+
 	var message_str = message.toString(); //convert byte array to string
+	console.log(message_str, topic)
 	message_str = message_str.replace(/\n$/, ''); //remove new line
 	//payload syntax: clientID,topic,message
-	if (countInstances(message_str) != 1) {
-		console.log("Invalid payload");
-		} else {	
-		insert_message(topic, message_str, packet);
-		//console.log(message_arr);
-	}
+
+	insert_message(topic, message_str, packet);
+	//console.log(message_arr);
+
 };
 
 function mqtt_close() {
@@ -64,40 +68,30 @@ function mqtt_close() {
 ////////////////////////////////////////////////////
 ///////////////////// MYSQL ////////////////////////
 ////////////////////////////////////////////////////
-var mysql = require('mysql'); //https://www.npmjs.com/package/mysql
-//Create Connection
-var connection = mysql.createConnection({
-	host: Database_URL,
-	user: "newuser",
-	password: "mypassword",
-	database: "mydb"
-});
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database("./coasta.db");
+db.serialize(() => { //https://www.npmjs.com/package/mysql
+	//Create Connection
+	db.run("CREATE TABLE info (topic Text, message TEXT)");
+})
 
-connection.connect(function(err) {
-	if (err) throw err;
-	//console.log("Database Connected!");
-});
+// db.close()
 
 //insert a row into the tbl_messages table
 function insert_message(topic, message_str, packet) {
-	var message_arr = extract_string(message_str); //split a string into an array
-	var clientID= message_arr[0];
-	var message = message_arr[1];
-	var sql = "INSERT INTO ?? (??,??,??) VALUES (?,?,?)";
-	var params = ['tbl_messages', 'clientID', 'topic', 'message', clientID, topic, message];
-	sql = mysql.format(sql, params);	
-	
-	connection.query(sql, function (error, results) {
-		if (error) throw error;
-		console.log("Message added: " + message_str);
-	}); 
-};	
+	db.serialize(() => { //https://www.npmjs.com/package/mysql
+		//Create Connection
+		const stmt = db.prepare("INSERT INTO info VALUES (?, ?)");
+		var params = [topic, message_str];
+		stmt.run(params);
+		stmt.finalize();
+	})
+
+	// db.close();
+};
 
 //split a string into an array of substrings
-function extract_string(message_str) {
-	var message_arr = message_str.split(","); //convert to array	
-	return message_arr;
-};	
+
 
 //count number of delimiters in a string
 var delimiter = ",";
