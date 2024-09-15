c6d0ec62838b956912af5aed1222b7cf1bfadfea
pgcr71 <pgcr71@gmail.com>
2024-09-15 23:36:45 +0530

    Feat: utilfacts sync

--- cronjob.js
+++ cronjob.js
@@ -8,18 +8,18 @@ const sqlite = verbose();
 // cron.schedule('1 */1 * * *',crons );
 crons()
 function crons() {
- 
+
     let lastHour = dayjs().subtract(1, 'hours').format("DDMMYYYYHH");
     let databasePath = `./databases/${lastHour}.db`;
     const db = new sqlite.Database(databasePath);
-        db.serialize(() => {
-            //Create Connection
-            getTopRecordForEachParameter(db)
-        })
-        //sync databases
-    
+    db.serialize(() => {
+        //Create Connection
+        getTopRecordForEachParameter(db)
+    })
+    //sync databases
+
 }
- function getTopRecordForEachParameter(db) {
+function getTopRecordForEachParameter(db) {
     db.all(`WITH summary AS (select *, dense_rank() OVER(PARTITION BY json_extract(value, '$.parameter'),json_extract(value, '$.mac'),json_extract(value, '$.sid')
 ORDER BY json_extract(value, '$.seq') desc) AS rank from info, json_each(info.message, '$'))
 select value from summary where rank = 1;`, async (err, res) => {
@@ -27,17 +27,22 @@ select value from summary where rank = 1;`, async (err, res) => {
             console.log('some thing went wrong')
             return
         }
-   
-        fetch('https://utilfacts.com/api/v1/sync',
-            {
-                method: 'POST',
-                headers: {
-                    'Authorization': "basic "+  btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD)
-                },
-                
-                body: JSON.stringify({data: res.map(({value}) => value)})
-            }).catch(e => console.log(e))
-    
+        try {
+            const result = await fetch('https://utilfacts.com/api/v1/sync',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': "basic " + btoa(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD)
+                    },
+
+                    body: JSON.stringify(res.map(({ value }) => JSON.parse(value)))
+                })
+            console.log(result)
+        } catch (e) {
+            console.log(e)
+        }
+
+
     })
 }
 
